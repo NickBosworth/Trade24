2edae8b53b1583c12ff7a5d5b107c923bf148406
Nick Bosworth <nick.bosworth@live.co.uk>
2023-10-14 17:58:55 +0100

    Adding in first event capture system

---
+++ Trade24.Detection/Event.cs
@@ -0,0 +1,25 @@
+﻿namespace Trade24.Detection
+{
+    public class Event
+    {
+        public string Symbol { get; set; }
+        public DateTime Date { get; set; }
+        public Direction Direction { get; set; }
+        public EventType Type { get; set; }
+
+
+    }
+
+    public enum Direction
+    {
+        Bullish,
+        Bearish
+    }
+
+    public enum EventType
+    {
+        CloseCross50Sma,
+
+    }
+
+}
---
+++ Trade24.Detection/SMAEventDetectors.cs
@@ -0,0 +1,48 @@
+﻿using Trade24.Common.Data;
+
+namespace Trade24.Detection
+{
+    public class SMAEventDetectors
+    {
+        public List<Event> GetSma50Events(List<RichDailyStockData> data)
+        {
+            //filter the data to only those where 50 sma is not 0.
+            data = data.Where(d => d.SMA50 != 0).ToList();
+
+            var events = new List<Event>();
+
+            foreach (var item in data)
+            {
+                if (item == data.First())
+                {
+                    continue;
+                }
+
+                if (item.Close > item.SMA50 && data[data.IndexOf(item) - 1].Close < data[data.IndexOf(item) - 1].SMA50)
+                {
+                    events.Add(new Event
+                    {
+                        Symbol = item.Symbol,
+                        Date = item.Date,
+                        Direction = Direction.Bullish,
+                        Type = EventType.CloseCross50Sma
+                    });
+                }
+                else if (item.Close < item.SMA50 && data[data.IndexOf(item) - 1].Close > data[data.IndexOf(item) - 1].SMA50)
+                {
+                    events.Add(new Event
+                    {
+                        Symbol = item.Symbol,
+                        Date = item.Date,
+                        Direction = Direction.Bearish,
+                        Type = EventType.CloseCross50Sma
+                    });
+                }
+
+
+            }
+
+            return events;
+        }
+    }
+}
